2d1eae8a899ade2b3e83f0b3763b40fdffc2133c
Adarsh Kumar <adarshkumar245@gmail.com>
2021-06-22 10:40:07 +0530

    add restart button

--- script.js
+++ script.js
@@ -4,17 +4,31 @@ const Game = (function(doc){
     var isOver = false;
     var gridArray = [];
     var result = doc.querySelector(".result h2");
+    var restartButton = doc.querySelector(".button");
+    var gridItems;
+
+    const reset=()=>{
+       gridItems.forEach((item)=>{
+           item.textContent = "";
+       });
+       restartButton.style.display = "none";
+       result.textContent = "Result:";
+       isX = true;
+       isOver = false;       
+    }
+    
     const start = () => {
-        const gridItems = document.querySelectorAll(".grid-item");
+        gridItems = document.querySelectorAll(".grid-item");
         gridItems.forEach(item => {
             item.addEventListener("click", ()=>{
              if(item.textContent!==""||isOver) return;
               input(item); 
             });
-        });         
+        });
+        restartButton.addEventListener("click", reset);         
     }
 
-    const populateArray = () => {
+    const updateGameboard = () => {
         gridArray = Array.from(doc.querySelectorAll(".grid-item")).map((item)=>{
             return item.textContent;
         });
@@ -27,31 +41,29 @@ const Game = (function(doc){
          item.textContent = "O";
          isX = !isX;
       }
-      populateArray();
-      console.log(gridArray);        
+      updateGameboard();        
       if(checkWinner(item.textContent)){
           isOver = true;
+          result.textContent +=item.textContent + " Won";
+          restartButton.style.display = "block";
           return;
       }else if(gridArray.every(item=>item!=="")){
          isOver = true;
          result.textContent += "Draw";
+         restartButton.style.display = "block";
       }
     }
     
     const checkWinner = (ch) => { 
-     if((gridArray[0]===ch&&gridArray[1]===ch&&gridArray[2]===ch)
-     ||(gridArray[3]===ch&&gridArray[4]===ch&&gridArray[5]===ch)
-     ||(gridArray[6]===ch&&gridArray[7]===ch&&gridArray[8]===ch)
-         ||(gridArray[0]===ch&&gridArray[3]===ch&&gridArray[6]===ch)
-         ||(gridArray[1]===ch&&gridArray[4]===ch&&gridArray[7]===ch)
-         ||(gridArray[2]===ch&&gridArray[5]===ch&&gridArray[8]===ch)
-         ||(gridArray[0]===ch&&gridArray[4]===ch&&gridArray[8]===ch)
-         ||(gridArray[2]===ch&&gridArray[4]===ch&&gridArray[6]===ch) 
-       ){
-            result.textContent+= ch +" Won";
-            return true;
-     }
-     return false; 
+      const horizontal = [0,3,6].map(i=>[i,i+1,i+2]);
+      const vertical = [0,1,2].map(i=>[i,i+3,i+6]);
+      const diagonal = [[0,4,8],[2,4,6]];
+
+      var allWins = [].concat(horizontal).concat(vertical).concat(diagonal);
+      let res = allWins.some((indices)=>{
+          return gridArray[indices[0]]==ch&&gridArray[indices[1]]===ch&&gridArray[indices[2]]==ch      
+      })
+      return res;
     }
     return {start};
  }(document));
