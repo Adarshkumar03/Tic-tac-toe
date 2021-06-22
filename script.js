ec7b702fcf2af919230d5cde4bc76f30e6bb0acb
Adarsh Kumar <adarshkumar245@gmail.com>
2021-06-22 20:02:40 +0530

    add form to add player names

--- script.js
+++ script.js
@@ -3,16 +3,21 @@ const Game = (function(doc){
     var isX = true;
     var isOver = false;
     var gridArray = [];
-    var result = doc.querySelector(".result h2");
+    var result = doc.querySelector(".result");
     var restartButton = doc.querySelector(".button");
     var gridItems;
+    var submitButton = doc.querySelector(".submit");
+    var form = doc.querySelector(".form");
+    var container = doc.querySelector(".container");
+    var player1 = "";
+    var player2 = "";
 
     const reset=()=>{
        gridItems.forEach((item)=>{
            item.textContent = "";
        });
        restartButton.style.display = "none";
-       result.textContent = "Result:";
+       result.textContent = player1 + "'s turn";
        isX = true;
        isOver = false;       
     }
@@ -25,7 +30,17 @@ const Game = (function(doc){
               input(item); 
             });
         });
-        restartButton.addEventListener("click", reset);         
+        restartButton.addEventListener("click", reset);
+        submitButton.addEventListener("click", setGameBoard);         
+    }
+
+    const setGameBoard = () => {
+        player1 = form.elements.player1.value;
+        player2 = form.elements.player2.value;
+       form.remove();
+       container.style.display = "grid";
+       result.style.display="block";
+       result.textContent = player1+"'s turn";
     }
 
     const updateGameboard = () => {
@@ -35,21 +50,27 @@ const Game = (function(doc){
     }
     const input = (item) => { 
      if(isX){
+         result.textContent = player2 + "'s turn";
          item.textContent = "X";
          isX = !isX;}
       else{
+         result.textContent = player1 + "'s turn"; 
          item.textContent = "O";
          isX = !isX;
       }
       updateGameboard();        
       if(checkWinner(item.textContent)){
           isOver = true;
-          result.textContent +=item.textContent + " Won";
+          if(item.textContent === "X"){
+              result.textContent=player1 + " Wins";
+          }else{
+            result.textContent=player2 + " Wins";
+          }
           restartButton.style.display = "block";
           return;
       }else if(gridArray.every(item=>item!=="")){
          isOver = true;
-         result.textContent += "Draw";
+         result.textContent += "Game Drawn";
          restartButton.style.display = "block";
       }
     }
