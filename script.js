664363795784360afabcf18fde66e1f523680d2b
Adarsh Kumar <adarshkumar245@gmail.com>
2021-06-23 15:42:36 +0530

    add ai functionality

--- script.js
+++ script.js
@@ -1,51 +1,52 @@
-
 const Game = (function(doc){
     var isX = true;
     var isOver = false;
     var gridArray = [];
     var result = doc.querySelector(".result");
-    var restartButton = doc.querySelector(".button");
-    var gridItems;
+    var restartButton = doc.querySelector(".restart");
+    var gridItems = [...doc.querySelectorAll(".grid-item")];
     var submitButton = doc.querySelector(".submit");
     var form = doc.querySelector(".form");
     var container = doc.querySelector(".container");
+    var backButton = doc.querySelector(".back");
     var player1 = "";
     var player2 = "";
-    var isAI = false;
-    
+    var gameType = "";
+    var twoPlayer = doc.querySelector(".two-player"); 
+    var ai = doc.querySelector(".ai"); 
+      
     const reset=()=>{
        gridItems.forEach((item)=>{
            item.textContent = "";
        });
        restartButton.style.display = "none";
-       result.textContent = player1 + "'s turn";
+       result.textContent = "";
        isX = true;
        isOver = false;       
     }
     
     const start = () => {
-        gridItems = document.querySelectorAll(".grid-item");
-        gridItems.forEach(item => {
-            item.addEventListener("click", ()=>{
-             if(item.textContent!==""||isOver) return;
-              input(item); 
-            });
-        });
+        container.style.display = "none";
         restartButton.addEventListener("click", reset);
-        submitButton.addEventListener("click", setGameBoard);         
+        twoPlayer.addEventListener("click", ()=>{
+           form.style.opacity = "1";
+           gameType = "two-player";       
+        })
+        submitButton.addEventListener("click", mode2);
+
+        ai.addEventListener("click", ()=>{
+            form.style.display = "none";
+            form.reset();
+            container.style.display = "grid";
+            result.style.display = "block";
+            basicAI();
+         })          
     }
 
-    const setGameBoard = () => {
-        player1 = form.elements.player1.value;
-        player2 = form.elements.player2.value;
-       form.remove();
-       container.style.display = "grid";
-       result.style.display="block";
-       result.textContent = player1+"'s turn";
-    }
+    
 
     const updateGameboard = () => {
-        gridArray = Array.from(doc.querySelectorAll(".grid-item")).map((item)=>{
+        gridArray = [...doc.querySelectorAll(".grid-item")].map((item)=>{
             return item.textContent;
         });
     }
@@ -54,33 +55,87 @@ const Game = (function(doc){
        return gridItems.filter((item)=> item.textContent==="");          
     } 
 
-    const input = (item) => { 
-     if(isX){
-         result.textContent = player2 + "'s turn";
-         item.textContent = "X";
-         isX = !isX;}
-      else{
-         result.textContent = player1 + "'s turn"; 
-         item.textContent = "O";
-         isX = !isX;
-      }
-      updateGameboard();        
-      if(checkWinner(item.textContent)){
-          isOver = true;
-          if(item.textContent === "X"){
-              result.textContent=player1 + " Wins";
-          }else{
-            result.textContent=player2 + " Wins";
-          }
-          restartButton.style.display = "block";
-          return;
-      }else if(gridArray.every(item=>item!=="")){
-         isOver = true;
-         result.textContent += "Game Drawn";
-         restartButton.style.display = "block";
-      }
+    function basicAI(){
+        console.log(gridItems);
+        gridItems.forEach((item)=>{
+            item.addEventListener("click", ()=>{
+               if(isX && item.textContent === "" && !isOver){
+                   item.textContent = "X";
+                   isX = !isX;
+                   if(!isX && emptyCells().length > 0 && !isOver){
+                       let random = Math.floor(Math.random()*emptyCells().length);
+                       let cell = emptyCells()[random];
+                       cell.textContent = "O";
+                       isX = !isX;
+                   }
+               }
+               updateGameboard();
+               if(checkWinner("X")){
+                result.textContent = "Human Wins";
+                isOver = true;
+                restartButton.style.display = "block";
+               }
+               if(checkWinner("O")){
+                result.textContent = "AI Wins";
+                isOver = true;
+                restartButton.style.display = "block";
+               }
+               if(isTie()){
+                   result.textContent = "It's a tie";
+                   isOver = true;
+                   restartButton.style.display = "block";
+               }
+              
+            })
+        })
     }
     
+    const isTie = ()=>{
+        return gridArray.every((item)=> {
+            return item.textContent === "X" || item.textContent === "O";
+        });
+    }
+
+    const mode2 = () => {
+        container.style.display = "grid";
+        result.style.display = "block";
+        player1 = form.elements.player1.value;
+        player2 = form.elements.player2.value;
+        form.style.display = "none";
+        form.reset();
+        result.textContent = player1+"'s turn";
+        gridItems.forEach((item)=>{
+            item.addEventListener("click", () => {
+               if(isOver || item.textContent!=="") return;
+               if(isX){
+                   item.textContent = "X";
+                   isX = !isX;
+                   result.textContent = player2 + "'s turn";
+               }else{
+                   item.textContent = "O";
+                   isX = !isX;
+                   result.textContent = player1 + "'s turn"
+               }
+               updateGameboard();
+               if(checkWinner("X")){
+                   result.textContent = player1 + " Wins";
+                   isOver = true;
+                   restartButton.style.display = "block";
+               }
+               if(checkWinner("O")){
+                result.textContent = player2 + " Wins";
+                isOver = true;
+                restartButton.style.display = "block";
+               }
+               if(isTie()){
+                result.textContent = "It's a Draw";
+                isOver = true;
+                restartButton.style.display = "block";
+               }
+            })
+        })
+    }
+
     const checkWinner = (ch) => { 
       const horizontal = [0,3,6].map(i=>[i,i+1,i+2]);
       const vertical = [0,1,2].map(i=>[i,i+3,i+6]);
@@ -88,7 +143,7 @@ const Game = (function(doc){
 
       var allWins = [].concat(horizontal).concat(vertical).concat(diagonal);
       let res = allWins.some((indices)=>{
-          return gridArray[indices[0]]==ch&&gridArray[indices[1]]===ch&&gridArray[indices[2]]==ch      
+          return gridArray[indices[0]]==ch&&gridArray[indices[1]]===ch&&gridArray[indices[2]]==ch
       })
       return res;
     }
